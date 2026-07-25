7128bdb309bfa11683e68c7e60fc6cb7812e50e6
cattosmile <238504882+cattosmile@users.noreply.github.com>
2026-07-25 19:42:26 +0200

    Meow

--- modules/home/desktop/hypr/hyprpaper/default.nix
+++ modules/home/desktop/hypr/hyprpaper/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  wallpaper1 = ../../../../../assets/walls/tracker.jpg;
+  wallpaper1 = ../../../../../assets/walls/arschlinux.jpg;
   wallpaper2 = ../../../../../assets/walls/black.png;
 in
 
